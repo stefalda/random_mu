f67b632f36edb706dfc18e7485bbd367c4dec9a4
stefalda <stefalda@users.noreply.github.com>
2025-02-05 09:07:52 +0000

    Deploying to gh-pages from @ stefalda/random_mu@91e60494a002e1cad5fcb932eeb69b4c1a36e89e 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3755890707"
+    serviceWorkerVersion: "3774927578"
   }
 });
